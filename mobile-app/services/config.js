309503c66c00091accaf016f6c2a346010145bb2
JonathanSeguraXP <jonathansegura.cv@gmail.com>
2026-05-17 00:21:01 -0600

    cambio de ip

--- mobile-app/services/config.js
+++ mobile-app/services/config.js
@@ -1,3 +1,3 @@
-const API_URL = 'http://192.168.1.106:4000/api';
-export const IMAGES_URL = 'http://192.168.1.106:4000';
+const API_URL = 'http://192.168.1.2:4000/api';
+export const IMAGES_URL = 'http://192.168.1.2:4000';
 export default API_URL;
