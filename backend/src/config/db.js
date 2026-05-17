12505afc73afd4a840503d8872b19844121fbf84
JonathanSeguraXP <jonathansegura.cv@gmail.com>
2026-05-17 01:41:16 -0600

    Update db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,11 +2,11 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || 'cafeteria_inventory',
-    port: process.env.DB_PORT || 3306,
+    host: process.DB_HOST || 'localhost',
+    user: process.DB_USER || 'root',
+    password: process.DB_PASSWORD || '',
+    database: process.DB_NAME || 'cafeteria_inventory',
+    port: process.DB_PORT || 3306,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
